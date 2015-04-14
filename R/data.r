5b67f18126427715489c3243f20e8719289be006
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-04-14 22:15:08 +0200

    Added sotu.dtm

--- R/data.r
+++ R/data.r
@@ -13,5 +13,25 @@ NULL
 #' @keywords datasets
 #' @name sotu
 #' @usage data(sotu)
-#' @format sotu.dtm: A dataframe of tokens produced by using Stanford CoreNLP to lemmatize the State of the Union speeches; and sotu.meta the metadata for these documents
+#' @format sotu.tokens: A dataframe of tokens produced by using Stanford CoreNLP to lemmatize the State of the Union speeches; and sotu.meta the metadata for these documents
+NULL
+
+
+#' Document metadata from Bush and Obama's State of the Union addresses
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name sotu
+#' @usage data(sotu)
+#' @format sotu-meta: A dataframe of document metadata matching the tokens from sotu.tokens
+NULL
+
+
+#' Document-term matrix containing the nouns, names and adjectives from Bush and Obama's State of the Union addresses
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name sotu
+#' @usage data(sotu)
+#' @format sotu.dtm: A document-term matrix based on sotu.tokens, containing only names, nouns, and adjectives
 NULL
